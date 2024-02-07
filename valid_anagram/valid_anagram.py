c64e2ca2f169963fdd3408552c58c7a7d226f3ce
MicaelTargino <micaelgomestargino@gmail.com>
2024-02-06 21:39:57 -0300

    v2 of valid anagram

--- valid_anagram/valid_anagram.py
+++ valid_anagram/valid_anagram.py
@@ -1,25 +1,17 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        map_s = {}
-        for element in list(s):
-            if map_s.get(element):
-                map_s[element] = map_s[element] + 1
-            else:
-                map_s[element] = 1
-        map_t = {}
-        for element in list(t):
-            if map_t.get(element):
-                map_t[element] = map_t[element] + 1
-            else:
-                map_t[element] = 1
-
         if len(s) != len(t):
             return False
 
-        for item in map_t.items():
-            if map_s.get(item[0]) != item[1]:
-                return False
-                
-        return True
+        char_count = {}
 
+        for i in range(len(s)):
+            char_count[s[i]] = char_count.get(s[i], 0) + 1
+            char_count[t[i]] = char_count.get(t[i], 0) - 1
+
+        for count in char_count.values():
+            if count != 0:
+                return False
+
+        return True
         
