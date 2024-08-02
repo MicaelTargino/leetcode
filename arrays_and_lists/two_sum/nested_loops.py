30f81e3c3bef7c483e2407cca55b0784dae1934b
MicaelTargino <micaelgomestargino@gmail.com>
2024-08-02 14:25:07 -0300

    refactor folder structure

--- arrays_and_lists/two_sum/nested_loops.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for idx1, num in enumerate(nums):
-            n1 = num
-            for idx2, n2 in enumerate(nums):
-                if n1 + n2 == target and idx1 != idx2:
-                    return [idx1, idx2]
-        return []
